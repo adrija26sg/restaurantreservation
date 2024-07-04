18ea0835c85c2c5f2f5ed3011db0d211442bb382
adrija26sg <adrija26sg@gmail.com>
2024-07-04 11:52:19 +0530

    changed in both frontend and backend folder

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -1,14 +1,21 @@
-import mongoose from "mongoose";
+// backend/database/dbConnection.js
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: './config.env' });
+
+console.log('MONGO_URI:', process.env.MONGO_URI); // Debugging line
 
 export const dbConnection = () => {
-  mongoose
-    .connect(process.env.MONGO_URI, {
-      dbName: "fooddata",
-    })
-    .then(() => {
-      console.log("Connected to database!");
-    })
-    .catch((err) => {
-      console.log(`Some error occured while connecing to database: ${err}`);
-    });
+  mongoose.connect(process.env.MONGO_URI, {
+    dbName: 'fooddata',
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to database!');
+  })
+  .catch((err) => {
+    console.log(`Some error occurred while connecting to the database: ${err}`);
+  });
 };
